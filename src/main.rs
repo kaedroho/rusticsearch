95ce36c3c33a8a20c04565e723509dab59f68314
Karl Hobley <karlhobley10@gmail.com>
2015-11-29 18:48:24 +0000

    Made all view functions closures
    So we can give them access to indices

--- src/main.rs
+++ src/main.rs
@@ -25,13 +25,11 @@ fn main() {
 
     let mut router = Router::new();
 
-    fn index(_: &mut Request) -> IronResult<Response> {
+    router.get("/", |_: &mut Request| -> IronResult<Response> {
         Ok(Response::with((status::Ok, "Hello World!")))
-    }
+    });
 
-    router.get("/", index);
-
-    fn count(req: &mut Request) -> IronResult<Response> {
+    router.get("/:index/_count", |req: &mut Request| -> IronResult<Response> {
         let ref index = req.extensions.get::<Router>().unwrap().find("index").unwrap_or("");
 
         let mut payload = String::new();
@@ -42,11 +40,9 @@ fn main() {
         let mut response = Response::with((status::Ok, "{\"count\": 0}"));
         response.headers.set_raw("Content-Type", vec![b"application/json".to_vec()]);
         Ok(response)
-    }
+    });
 
-    router.get("/:index/_count", count);
-
-    fn search(req: &mut Request) -> IronResult<Response> {
+    router.get("/:index/_search", |req: &mut Request| -> IronResult<Response> {
         let ref index = req.extensions.get::<Router>().unwrap().find("index").unwrap_or("");
 
         let mut payload = String::new();
@@ -57,11 +53,9 @@ fn main() {
         let mut response = Response::with((status::Ok, "{\"hits\": {\"total\": 0, \"hits\": []}}"));
         response.headers.set_raw("Content-Type", vec![b"application/json".to_vec()]);
         Ok(response)
-    }
+    });
 
-    router.get("/:index/_search", search);
-
-    fn put(req: &mut Request) -> IronResult<Response> {
+    router.put("/:index/:mapping/:doc", |req: &mut Request| -> IronResult<Response> {
         let ref index = req.extensions.get::<Router>().unwrap().find("index").unwrap_or("");
         let ref mapping = req.extensions.get::<Router>().unwrap().find("mapping").unwrap_or("");
         let ref doc = req.extensions.get::<Router>().unwrap().find("doc").unwrap_or("");
@@ -74,9 +68,7 @@ fn main() {
         let mut response = Response::with((status::Ok, "{}"));
         response.headers.set_raw("Content-Type", vec![b"application/json".to_vec()]);
         Ok(response)
-    }
-
-    router.put("/:index/:mapping/:doc", put);
+    });
 
     Iron::new(router).http("localhost:9200").unwrap();
 }
