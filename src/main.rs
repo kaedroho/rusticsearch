a7be762250cdc22fe466b1b0d92c922d8107e4c0
Karl Hobley <karlhobley10@gmail.com>
2016-01-02 21:43:51 +0000

    Moved filters into separate file

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ extern crate persistent;
 extern crate rustc_serialize;
 
 mod views;
+mod filters;
 
 use std::io::Read;
 use std::sync::{Arc, Mutex};
@@ -26,53 +27,6 @@ impl Document {
     fn from_json(data: Json) -> Document {
         Document{
             data: data,
-        }
-    }
-}
-
-
-#[derive(Debug)]
-enum Filter {
-    Not(Box<Filter>),
-    Or(Vec<Filter>),
-    And(Vec<Filter>),
-    Term(String, String),
-}
-
-
-impl Filter {
-    fn matches(&self, doc: &Document) -> bool {
-        match *self {
-            Filter::Not(ref filter) => !filter.matches(doc),
-            Filter::Or(ref filters) => {
-                for filter in filters.iter() {
-                    if (filter.matches(doc)) {
-                        return true;
-                    }
-                }
-
-                false
-            },
-            Filter::And(ref filters) => {
-                for filter in filters.iter() {
-                    if (!filter.matches(doc)) {
-                        return false;
-                    }
-                }
-
-                true
-            },
-            Filter::Term(ref field, ref value) => {
-                let obj = doc.data.as_object().unwrap();
-
-                if let Some(field_value) = obj.get(field) {
-                    if let Json::String(ref field_value) = *field_value {
-                        return field_value == value
-                    }
-                }
-
-                false
-            }
         }
     }
 }
