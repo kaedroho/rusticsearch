a44ab86a5f48a9e97df5539f8d0a05ff3da3ca6e
Karl Hobley <karlhobley10@gmail.com>
2016-05-13 13:02:06 +0100

    Added get_query_parser function

--- src/query/parser/mod.rs
+++ src/query/parser/mod.rs
@@ -7,9 +7,23 @@ use query::Query;
 
 #[derive(Debug, PartialEq)]
 pub enum QueryParseError {
+    UnrecognisedQueryType(String),
+}
+
+
+fn get_query_parser(query_name: &str) -> Option<fn(&Json) -> Result<Query, QueryParseError>> {
+    match query_name {
+        "match" => Some(match_query::parse),
+        _ => None
+    }
 }
 
 
 pub fn parse(json: &Json) -> Result<Query, QueryParseError> {
-    Ok(Query::MatchNone)
+    let query_type = "match".to_owned();
+
+    match get_query_parser(&query_type) {
+        Some(parse) => parse(json),
+        None => Err(QueryParseError::UnrecognisedQueryType(query_type)),
+    }
 }
