fcc5c8ee193232ccde07c93761f37a232e3a50dc
Karl Hobley <karlhobley10@gmail.com>
2016-07-29 14:36:29 +0100

    Implemented TotalCountCollector

--- abra/src/collectors/mod.rs
+++ abra/src/collectors/mod.rs
@@ -1,4 +1,41 @@
+pub mod total_count;
+
+
+#[derive(Debug)]
+pub struct DocumentMatch {
+    id: u64,
+    score: Option<f64>,
+}
+
+
+impl DocumentMatch {
+    pub fn new_unscored(id: u64) -> DocumentMatch {
+        DocumentMatch {
+            id: id,
+            score: None,
+        }
+    }
+
+    pub fn new_scored(id: u64, score: f64) -> DocumentMatch {
+        DocumentMatch {
+            id: id,
+            score: Some(score),
+        }
+    }
+
+    #[inline]
+    pub fn doc_id(&self) -> u64 {
+        self.id
+    }
+
+    #[inline]
+    pub fn score(&self) -> Option<f64> {
+        self.score
+    }
+}
+
+
 pub trait Collector {
     fn needs_score(&self) -> bool;
-    fn collect(&mut self, doc: u64);
+    fn collect(&mut self, doc: DocumentMatch);
 }
