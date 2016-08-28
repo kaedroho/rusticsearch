6fb56d022f8b92d69b0d6ecd373ce64523778615
Karl Hobley <karlhobley10@gmail.com>
2016-08-28 11:37:17 +0100

    Added test for alias API

--- tests/tests.py
+++ tests/tests.py
@@ -21,5 +21,26 @@ class TestIndex(RusticsearchTestCase):
         self.assertFalse(self.es.indices.exists(index='foo'))
 
 
+class TestAlias(RusticsearchTestCase):
+    def test_put_alias_exists_and_delete(self):
+        # Create index
+        self.es.indices.create(index='foo')
+
+        # Create alias
+        self.es.indices.put_alias(index='foo', name='alias')
+
+        # Check the alias exists
+        self.assertTrue(self.es.indices.exists(index='alias'))
+        self.assertTrue(self.es.indices.exists_alias(name='alias'))
+        self.assertTrue(self.es.indices.exists_alias(index='foo', name='alias'))
+        self.assertFalse(self.es.indices.exists_alias(index='bar', name='alias'))
+
+        # Now delete the index by the alias
+        self.es.indices.delete(index='alias')
+        self.assertFalse(self.es.indices.exists(index='alias'))
+        self.assertFalse(self.es.indices.exists_alias(name='alias'))
+        self.assertFalse(self.es.indices.exists(index='foo'))
+
+
 if __name__ == '__main__':
     unittest.main()
