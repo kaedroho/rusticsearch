1cf41c5464e10b3ae28ce5275ada825e43ad0ea4
Karl Hobley <karlhobley10@gmail.com>
2016-09-04 16:43:10 +0100

    Tests

--- src/rusticsearch/index/settings.rs
+++ src/rusticsearch/index/settings.rs
@@ -8,6 +8,7 @@ use analysis::tokenizers::TokenizerSpec;
 use analysis::filters::FilterSpec;
 
 
+#[derive(Debug)]
 pub struct IndexSettings {
     pub tokenizers: HashMap<String, TokenizerSpec>,
     pub filters: HashMap<String, FilterSpec>,
@@ -26,8 +27,11 @@ impl IndexSettings {
 }
 
 
+#[derive(Debug, PartialEq)]
 pub enum IndexSettingsParseError {
     SomethingWentWrong,
+    UnrecognisedTokenizerType(String),
+    UnrecognisedFilterType(String),
 }
 
 
@@ -136,7 +140,7 @@ fn parse_tokenizer(data: &Json) -> Result<TokenizerSpec, IndexSettingsParseError
         // pattern
         // classic
         // thai
-        _ => Err(IndexSettingsParseError::SomethingWentWrong),
+        _ => Err(IndexSettingsParseError::UnrecognisedTokenizerType(tokenizer_type.to_owned())),
     }
 }
 
@@ -289,7 +293,7 @@ fn parse_filter(data: &Json) -> Result<FilterSpec, IndexSettingsParseError> {
         // classic
         // decimal_digit
         // fingerprint
-        _ => Err(IndexSettingsParseError::SomethingWentWrong),
+        _ => Err(IndexSettingsParseError::UnrecognisedFilterType(filter_type.to_owned())),
     }
 }
 
@@ -472,4 +476,196 @@ pub fn parse(data: Json) -> Result<IndexSettings, IndexSettingsParseError> {
 
 
     Ok(index_settings)
+}
+
+
+#[cfg(test)]
+mod tests {
+    use rustc_serialize::json::Json;
+
+    use abra::analysis::ngram_generator::Edge;
+    use abra::analysis::tokenizers::TokenizerSpec;
+    use abra::analysis::filters::FilterSpec;
+
+    use super::{parse, IndexSettingsParseError};
+
+    #[test]
+    fn test_empty() {
+        let settings = parse(Json::from_str("
+        {
+        }
+        ").unwrap());
+
+        let settings = settings.expect("parse() returned an error");
+
+        assert_eq!(settings.tokenizers.len(), 0);
+        assert_eq!(settings.filters.len(), 0);
+        assert_eq!(settings.analyzers.len(), 0);
+    }
+
+    #[test]
+    fn test_custom_analyser() {
+        let settings = parse(Json::from_str("
+        {
+            \"settings\": {
+                \"analysis\": {
+                    \"tokenizer\": {
+                        \"ngram_tokenizer\": {
+                            \"type\": \"nGram\",
+                            \"min_gram\": 3,
+                            \"max_gram\": 15
+                        },
+                        \"edgengram_tokenizer\": {
+                            \"type\": \"edgeNGram\",
+                            \"min_gram\": 2,
+                            \"max_gram\": 15
+                        },
+                        \"edgengram_tokenizer_side_front\": {
+                            \"type\": \"edgeNGram\",
+                            \"min_gram\": 2,
+                            \"max_gram\": 15,
+                            \"side\": \"front\"
+                        },
+                        \"edgengram_tokenizer_side_back\": {
+                            \"type\": \"edgeNGram\",
+                            \"min_gram\": 2,
+                            \"max_gram\": 15,
+                            \"side\": \"back\"
+                        }
+                    },
+                    \"filter\": {
+                        \"ngram_filter\": {
+                            \"type\": \"nGram\",
+                            \"min_gram\": 3,
+                            \"max_gram\": 15
+                        },
+                        \"edgengram_filter\": {
+                            \"type\": \"edgeNGram\",
+                            \"min_gram\": 2,
+                            \"max_gram\": 15
+                        },
+                        \"edgengram_filter_side_front\": {
+                            \"type\": \"edgeNGram\",
+                            \"min_gram\": 2,
+                            \"max_gram\": 15,
+                            \"side\": \"front\"
+                        },
+                        \"edgengram_filter_side_back\": {
+                            \"type\": \"edgeNGram\",
+                            \"min_gram\": 2,
+                            \"max_gram\": 15,
+                            \"side\": \"back\"
+                        }
+                    }
+                }
+            }
+        }
+        ").unwrap());
+
+        let settings = settings.expect("parse() returned an error");
+
+        assert_eq!(settings.tokenizers.len(), 4);
+        assert_eq!(settings.filters.len(), 4);
+        assert_eq!(settings.analyzers.len(), 0);
+
+        // Check tokenizers
+        let ngram_tokenizer = settings.tokenizers.get("ngram_tokenizer").expect("'ngram_tokenizer' wasn't created");
+        assert_eq!(*ngram_tokenizer, TokenizerSpec::NGram {
+            min_size: 3,
+            max_size: 15,
+            edge: Edge::Neither,
+        });
+
+        let edgengram_tokenizer = settings.tokenizers.get("edgengram_tokenizer").expect("'edgengram_tokenizer' wasn't created");
+        assert_eq!(*edgengram_tokenizer, TokenizerSpec::NGram {
+            min_size: 2,
+            max_size: 15,
+            edge: Edge::Left,
+        });
+
+        let edgengram_tokenizer_side_front = settings.tokenizers.get("edgengram_tokenizer_side_front").expect("'edgengram_tokenizer_side_front' wasn't created");
+        assert_eq!(*edgengram_tokenizer_side_front, TokenizerSpec::NGram {
+            min_size: 2,
+            max_size: 15,
+            edge: Edge::Left,
+        });
+
+        let edgengram_tokenizer_side_back = settings.tokenizers.get("edgengram_tokenizer_side_back").expect("'edgengram_tokenizer_side_back' wasn't created");
+        assert_eq!(*edgengram_tokenizer_side_back, TokenizerSpec::NGram {
+            min_size: 2,
+            max_size: 15,
+            edge: Edge::Right,
+        });
+
+        // Check filters
+        let ngram_filter = settings.filters.get("ngram_filter").expect("'ngram_filter' wasn't created");
+        assert_eq!(*ngram_filter, FilterSpec::NGram {
+            min_size: 3,
+            max_size: 15,
+            edge: Edge::Neither,
+        });
+
+        let edgengram_filter = settings.filters.get("edgengram_filter").expect("'edgengram_filter' wasn't created");
+        assert_eq!(*edgengram_filter, FilterSpec::NGram {
+            min_size: 2,
+            max_size: 15,
+            edge: Edge::Left,
+        });
+
+        let edgengram_filter_side_front = settings.filters.get("edgengram_filter_side_front").expect("'edgengram_filter_side_front' wasn't created");
+        assert_eq!(*edgengram_filter_side_front, FilterSpec::NGram {
+            min_size: 2,
+            max_size: 15,
+            edge: Edge::Left,
+        });
+
+        let edgengram_filter_side_back = settings.filters.get("edgengram_filter_side_back").expect("'edgengram_filter_side_back' wasn't created");
+        assert_eq!(*edgengram_filter_side_back, FilterSpec::NGram {
+            min_size: 2,
+            max_size: 15,
+            edge: Edge::Right,
+        });
+    }
+
+    #[test]
+    fn test_custom_analyser_bad_tokenizer_type() {
+        let settings = parse(Json::from_str("
+        {
+            \"settings\": {
+                \"analysis\": {
+                    \"tokenizer\": {
+                        \"bad_tokenizer\": {
+                            \"type\": \"foo\"
+                        }
+                    }
+                }
+            }
+        }
+        ").unwrap());
+
+        let error = settings.err().expect("parse() was supposed to return an error, but didn't");
+
+        assert_eq!(error, IndexSettingsParseError::UnrecognisedTokenizerType("foo".to_string()))
+    }
+
+    #[test]
+    fn test_custom_analyser_bad_filter_type() {
+        let settings = parse(Json::from_str("
+        {
+            \"settings\": {
+                \"analysis\": {
+                    \"filter\": {
+                        \"bad_filter\": {
+                            \"type\": \"foo\"
+                        }
+                    }
+                }
+            }
+        }
+        ").unwrap());
+
+        let error = settings.err().expect("parse() was supposed to return an error, but didn't");
+
+        assert_eq!(error, IndexSettingsParseError::UnrecognisedFilterType("foo".to_string()))
+    }
 }
