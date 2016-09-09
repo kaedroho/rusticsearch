3d7f921ad0a6e40833e2aa200a81f41a63364502
Karl Hobley <karlhobley10@gmail.com>
2016-09-09 16:25:50 +0100

    Implemented index create/open methods

--- abra_rocksdb/src/lib.rs
+++ abra_rocksdb/src/lib.rs
@@ -1,6 +1,61 @@
+extern crate abra;
+extern crate rocksdb;
+
+use rocksdb::{DB, Options};
+
+
+pub struct RocksDBIndexStore {
+    db: DB,
+}
+
+
+impl RocksDBIndexStore {
+    pub fn create(path: &str) -> Result<RocksDBIndexStore, String> {
+        let mut opts = Options::default();
+        opts.create_if_missing(true);
+        let db = try!(DB::open(&opts, path));
+
+        // TODO: Initialise
+
+        Ok(RocksDBIndexStore {
+            db: db,
+        })
+    }
+
+    pub fn open(path: &str) -> Result<RocksDBIndexStore, String> {
+        let mut opts = Options::default();
+        let db = try!(DB::open(&opts, path));
+
+        Ok(RocksDBIndexStore {
+            db: db,
+        })
+    }
+}
+
+
 #[cfg(test)]
 mod tests {
+    use rocksdb::{DB, Options};
+
+    use super::RocksDBIndexStore;
+
     #[test]
-    fn it_works() {
+    fn test_create() {
+        let store = RocksDBIndexStore::create("test_indices/test_create");
+        assert!(store.is_ok());
+    }
+
+    #[test]
+    fn test_open() {
+        let store = RocksDBIndexStore::open("test_indices/test_open");
+        assert!(store.is_err());
+
+        // Create DB
+        let mut opts = Options::default();
+        opts.create_if_missing(true);
+        DB::open(&opts, "test_indices/test_open").unwrap();
+
+        let store = RocksDBIndexStore::open("test_indices/test_open");
+        assert!(store.is_ok());
     }
 }
