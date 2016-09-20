3e73bb4a2adc88244202d3cba1d3b538a142d5db
Karl Hobley <karlhobley10@gmail.com>
2016-09-20 22:50:43 +0100

    Docstrings for analysis module

--- abra/src/analysis/tokenizers/mod.rs
+++ abra/src/analysis/tokenizers/mod.rs
@@ -7,6 +7,26 @@ use analysis::tokenizers::standard::StandardTokenizer;
 use analysis::tokenizers::ngram::NGramTokenizer;
 
 
+/// Defines a tokenizer
+///
+/// You can use this to define a tokenizer before having to bind it to any data
+///
+/// # Examples
+///
+/// ```
+/// use abra::{Term, Token};
+/// use abra::analysis::tokenizers::TokenizerSpec;
+///
+/// let standard_tokenizer = TokenizerSpec::Standard;
+/// let token_stream = standard_tokenizer.initialise("Hello, world!");
+///
+/// let tokens = token_stream.collect::<Vec<Token>>();
+///
+/// assert_eq!(tokens, vec![
+///     Token { term: Term::String("Hello".to_string()), position: 1 },
+///     Token { term: Term::String("world".to_string()), position: 2 },
+/// ]);
+/// ```
 #[derive(Debug, Clone, PartialEq)]
 pub enum TokenizerSpec {
     Standard,
