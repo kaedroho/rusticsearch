8a241404e757d4c370409391f841644fd4ea2ef2
Karl Hobley <karlhobley10@gmail.com>
2016-09-28 22:00:08 +0100

    Cleanup in query planner

--- abra_rocksdb/src/search.rs
+++ abra_rocksdb/src/search.rs
@@ -16,40 +16,50 @@ enum BooleanQueryOp {
 }
 
 
+#[derive(Debug)]
 struct SearchPlan {
     boolean_query: Vec<BooleanQueryOp>,
 }
 
 
+impl SearchPlan {
+    fn new() -> SearchPlan {
+        SearchPlan {
+            boolean_query: Vec::new(),
+        }
+    }
+}
+
+
 impl<'a> RocksDBIndexReader<'a> {
-    fn plan_boolean_query_combinator(&self, mut boolean_query: &mut Vec<BooleanQueryOp>, queries: &Vec<Query>, join_op: BooleanQueryOp) {
+    fn plan_query_combinator(&self, mut plan: &mut SearchPlan, queries: &Vec<Query>, join_op: BooleanQueryOp) {
         match queries.len() {
-            0 => boolean_query.push(BooleanQueryOp::Zero),
+            0 => plan.boolean_query.push(BooleanQueryOp::Zero),
             1 => {
-                self.plan_boolean_query(&mut boolean_query, &queries[0]);
+                self.plan_query(&mut plan, &queries[0]);
             }
             _ => {
                 // TODO: organise queries into a binary tree structure
 
                 let mut query_iter = queries.iter();
-                self.plan_boolean_query(&mut boolean_query, query_iter.next().unwrap());
+                self.plan_query(&mut plan, query_iter.next().unwrap());
 
                 for query in query_iter {
-                    self.plan_boolean_query(&mut boolean_query, query);
-                    boolean_query.push(join_op.clone());
+                    self.plan_query(&mut plan, query);
+                    plan.boolean_query.push(join_op.clone());
                 }
             }
         }
     }
 
-    fn plan_boolean_query(&self, mut boolean_query: &mut Vec<BooleanQueryOp>, query: &Query) {
+    fn plan_query(&self, mut plan: &mut SearchPlan, query: &Query) {
         match *query {
             Query::MatchAll{ref score} => {
-                boolean_query.push(BooleanQueryOp::One);
-            },
+                plan.boolean_query.push(BooleanQueryOp::One);
+            }
             Query::MatchNone => {
-                boolean_query.push(BooleanQueryOp::Zero);
-            },
+                plan.boolean_query.push(BooleanQueryOp::Zero);
+            }
             Query::MatchTerm{ref field, ref term, ref matcher, ref scorer} => {
                 // Get term
                 let term_bytes = term.to_bytes();
@@ -57,7 +67,7 @@ impl<'a> RocksDBIndexReader<'a> {
                     Some(term_ref) => *term_ref,
                     None => {
                         // Term doesn't exist, so will never match
-                        boolean_query.push(BooleanQueryOp::Zero);
+                        plan.boolean_query.push(BooleanQueryOp::Zero);
                         return
                     }
                 };
@@ -67,45 +77,41 @@ impl<'a> RocksDBIndexReader<'a> {
                     Some(field_ref) => field_ref,
                     None => {
                         // Field doesn't exist, so will never match
-                        boolean_query.push(BooleanQueryOp::Zero);
+                        plan.boolean_query.push(BooleanQueryOp::Zero);
                         return
                     }
                 };
 
-                boolean_query.push(BooleanQueryOp::Load(field_ref, term_ref));
+                plan.boolean_query.push(BooleanQueryOp::Load(field_ref, term_ref));
             }
             Query::Conjunction{ref queries} => {
-                self.plan_boolean_query_combinator(&mut boolean_query, queries, BooleanQueryOp::And);
+                self.plan_query_combinator(&mut plan, queries, BooleanQueryOp::And);
             }
             Query::Disjunction{ref queries} => {
-                self.plan_boolean_query_combinator(&mut boolean_query, queries, BooleanQueryOp::Or);
+                self.plan_query_combinator(&mut plan, queries, BooleanQueryOp::Or);
             }
             Query::NDisjunction{ref queries, minimum_should_match} => {
-                self.plan_boolean_query_combinator(&mut boolean_query, queries, BooleanQueryOp::Or);
+                self.plan_query_combinator(&mut plan, queries, BooleanQueryOp::Or);
             }
             Query::DisjunctionMax{ref queries} => {
-                self.plan_boolean_query_combinator(&mut boolean_query, queries, BooleanQueryOp::Or);
+                self.plan_query_combinator(&mut plan, queries, BooleanQueryOp::Or);
             }
             Query::Filter{ref query, ref filter} => {
-                self.plan_boolean_query(&mut boolean_query, query);
-                self.plan_boolean_query(&mut boolean_query, filter);
-                boolean_query.push(BooleanQueryOp::And);
+                self.plan_query(&mut plan, query);
+                self.plan_query(&mut plan, filter);
+                plan.boolean_query.push(BooleanQueryOp::And);
             }
             Query::Exclude{ref query, ref exclude} => {
-                self.plan_boolean_query(&mut boolean_query, query);
-                self.plan_boolean_query(&mut boolean_query, exclude);
-                boolean_query.push(BooleanQueryOp::AndNot);
+                self.plan_query(&mut plan, query);
+                self.plan_query(&mut plan, exclude);
+                plan.boolean_query.push(BooleanQueryOp::AndNot);
             }
         }
     }
 
     pub fn search<C: Collector>(&self, collector: &mut C, query: &Query) {
-        let mut boolean_query = Vec::new();
-        self.plan_boolean_query(&mut boolean_query, query);
-        println!("BQ {:?}", boolean_query);
-
-        let plan = SearchPlan {
-            boolean_query: boolean_query,
-        };
+        let mut plan = SearchPlan::new();
+        self.plan_query(&mut plan, query);
+        println!("Plan {:?}", plan);
     }
 }
