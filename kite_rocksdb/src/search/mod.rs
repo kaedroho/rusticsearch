f324df0b3d6b493e8a219e73ed2a896ed7d86d18
Karl Hobley <karlhobley10@gmail.com>
2016-10-19 13:27:08 +0100

    Moved query planner into plan module

--- kite_rocksdb/src/search/mod.rs
+++ kite_rocksdb/src/search/mod.rs
@@ -15,124 +15,11 @@ use search::doc_id_set::DocIdSet;
 use search::statistics::StatisticsReader;
 use search::boolean_retrieval::BooleanQueryOp;
 use search::scoring::{CombinatorScorer, ScoreFunctionOp};
-use search::plan::SearchPlan;
+use search::plan::{SearchPlan, plan_query};
 use search::plan::boolean_query_builder::BooleanQueryBuilder;
 
 
 impl<'a> RocksDBIndexReader<'a> {
-    fn plan_query_combinator(&self, mut plan: &mut SearchPlan, queries: &Vec<Query>, join_op: BooleanQueryOp, score: bool, scorer: CombinatorScorer) {
-        match queries.len() {
-            0 => {
-                plan.boolean_query.push(BooleanQueryOp::PushEmpty);
-                plan.score_function.push(ScoreFunctionOp::Literal(0.0f64));
-            }
-            1 =>  self.plan_query(&mut plan, &queries[0], score),
-            _ => {
-                let mut query_iter = queries.iter();
-                self.plan_query(&mut plan, query_iter.next().unwrap(), score);
-
-                for query in query_iter {
-                    self.plan_query(&mut plan, query, score);
-                    plan.boolean_query.push(join_op.clone());
-                }
-            }
-        }
-
-        plan.score_function.push(ScoreFunctionOp::CombinatorScorer(queries.len() as u32, scorer));
-    }
-
-    fn plan_query(&self, mut plan: &mut SearchPlan, query: &Query, score: bool) {
-        match *query {
-            Query::MatchAll{ref score} => {
-                plan.boolean_query.push(BooleanQueryOp::PushFull);
-                plan.score_function.push(ScoreFunctionOp::Literal(*score));
-            }
-            Query::MatchNone => {
-                plan.boolean_query.push(BooleanQueryOp::PushEmpty);
-                plan.score_function.push(ScoreFunctionOp::Literal(0.0f64));
-            }
-            Query::MatchTerm{ref field, ref term, ref scorer} => {
-                // Get field
-                let field_ref = match self.schema().get_field_by_name(field) {
-                    Some(field_ref) => field_ref,
-                    None => {
-                        // Field doesn't exist, so will never match
-                        plan.boolean_query.push(BooleanQueryOp::PushEmpty);
-                        plan.score_function.push(ScoreFunctionOp::Literal(0.0f64));
-                        return
-                    }
-                };
-
-                // Get term
-                let term_bytes = term.to_bytes();
-                let term_ref = match self.store.term_dictionary.get(&term_bytes) {
-                    Some(term_ref) => term_ref,
-                    None => {
-                        // Term doesn't exist, so will never match
-                        plan.boolean_query.push(BooleanQueryOp::PushEmpty);
-                        plan.score_function.push(ScoreFunctionOp::Literal(0.0f64));
-                        return
-                    }
-                };
-
-                plan.boolean_query.push(BooleanQueryOp::PushTermDirectory(field_ref, term_ref));
-                plan.score_function.push(ScoreFunctionOp::TermScorer(field_ref, term_ref, scorer.clone()));
-            }
-            Query::MatchMultiTerm{ref field, ref term_selector, ref scorer} => {
-                // Get field
-                let field_ref = match self.schema().get_field_by_name(field) {
-                    Some(field_ref) => field_ref,
-                    None => {
-                        // Field doesn't exist, so will never match
-                        plan.boolean_query.push(BooleanQueryOp::PushEmpty);
-                        plan.score_function.push(ScoreFunctionOp::Literal(0.0f64));
-                        return
-                    }
-                };
-
-                // Get terms
-                plan.boolean_query.push(BooleanQueryOp::PushEmpty);
-                let mut total_terms = 0;
-                for term_ref in self.store.term_dictionary.select(term_selector) {
-                    plan.boolean_query.push(BooleanQueryOp::PushTermDirectory(field_ref, term_ref));
-                    plan.boolean_query.push(BooleanQueryOp::Or);
-                    plan.score_function.push(ScoreFunctionOp::TermScorer(field_ref, term_ref, scorer.clone()));
-                    total_terms += 1;
-                }
-
-                // This query must push only one score value onto the stack.
-                // If we haven't pushed any score operations, Push a literal 0.0
-                // If we have pushed more than one score operations, which will lead to more
-                // than one score value being pushed to the stack, combine the score values
-                // with a combinator operation.
-                match total_terms {
-                    0 => plan.score_function.push(ScoreFunctionOp::Literal(0.0f64)),
-                    1 => {},
-                    _ => plan.score_function.push(ScoreFunctionOp::CombinatorScorer(total_terms, CombinatorScorer::Avg)),
-                }
-            }
-            Query::Conjunction{ref queries} => {
-                self.plan_query_combinator(&mut plan, queries, BooleanQueryOp::And, score, CombinatorScorer::Avg);
-            }
-            Query::Disjunction{ref queries} => {
-                self.plan_query_combinator(&mut plan, queries, BooleanQueryOp::Or, score, CombinatorScorer::Avg);
-            }
-            Query::DisjunctionMax{ref queries} => {
-                self.plan_query_combinator(&mut plan, queries, BooleanQueryOp::Or, score, CombinatorScorer::Max);
-            }
-            Query::Filter{ref query, ref filter} => {
-                self.plan_query(&mut plan, query, score);
-                self.plan_query(&mut plan, filter, false);
-                plan.boolean_query.push(BooleanQueryOp::And);
-            }
-            Query::Exclude{ref query, ref exclude} => {
-                self.plan_query(&mut plan, query, score);
-                self.plan_query(&mut plan, exclude, false);
-                plan.boolean_query.push(BooleanQueryOp::AndNot);
-            }
-        }
-    }
-
     fn search_chunk_boolean_phase(&self, plan: &SearchPlan, chunk: u32) -> DocIdSet {
         // Execute boolean query
         let mut stack = Vec::new();
@@ -298,7 +185,7 @@ impl<'a> RocksDBIndexReader<'a> {
 
     pub fn search<C: Collector>(&self, collector: &mut C, query: &Query) {
         let mut plan = SearchPlan::new();
-        self.plan_query(&mut plan, query, true);
+        plan_query(&self, &mut plan, query, true);
 
         // Add operations to exclude deleted documents to boolean query
         plan.boolean_query.push(BooleanQueryOp::PushDeletionList);
--- kite_rocksdb/src/search/plan/mod.rs
+++ kite_rocksdb/src/search/plan/mod.rs
@@ -1,7 +1,10 @@
 pub mod boolean_query_builder;
 
+use kite::Query;
+
+use RocksDBIndexReader;
 use search::boolean_retrieval::BooleanQueryOp;
-use search::scoring::ScoreFunctionOp;
+use search::scoring::{CombinatorScorer, ScoreFunctionOp};
 
 
 #[derive(Debug)]
@@ -18,6 +21,121 @@ impl SearchPlan {
             boolean_query: Vec::new(),
             boolean_query_is_negated: false,
             score_function: Vec::new(),
+        }
+    }
+}
+
+
+fn plan_query_combinator(index_reader: &RocksDBIndexReader, mut plan: &mut SearchPlan, queries: &Vec<Query>, join_op: BooleanQueryOp, score: bool, scorer: CombinatorScorer) {
+    match queries.len() {
+        0 => {
+            plan.boolean_query.push(BooleanQueryOp::PushEmpty);
+            plan.score_function.push(ScoreFunctionOp::Literal(0.0f64));
+        }
+        1 =>  plan_query(index_reader, &mut plan, &queries[0], score),
+        _ => {
+            let mut query_iter = queries.iter();
+            plan_query(index_reader, &mut plan, query_iter.next().unwrap(), score);
+
+            for query in query_iter {
+                plan_query(index_reader, &mut plan, query, score);
+                plan.boolean_query.push(join_op.clone());
+            }
+        }
+    }
+
+    plan.score_function.push(ScoreFunctionOp::CombinatorScorer(queries.len() as u32, scorer));
+}
+
+
+pub fn plan_query(index_reader: &RocksDBIndexReader,mut plan: &mut SearchPlan, query: &Query, score: bool) {
+    match *query {
+        Query::MatchAll{ref score} => {
+            plan.boolean_query.push(BooleanQueryOp::PushFull);
+            plan.score_function.push(ScoreFunctionOp::Literal(*score));
+        }
+        Query::MatchNone => {
+            plan.boolean_query.push(BooleanQueryOp::PushEmpty);
+            plan.score_function.push(ScoreFunctionOp::Literal(0.0f64));
+        }
+        Query::MatchTerm{ref field, ref term, ref scorer} => {
+            // Get field
+            let field_ref = match index_reader.schema().get_field_by_name(field) {
+                Some(field_ref) => field_ref,
+                None => {
+                    // Field doesn't exist, so will never match
+                    plan.boolean_query.push(BooleanQueryOp::PushEmpty);
+                    plan.score_function.push(ScoreFunctionOp::Literal(0.0f64));
+                    return
+                }
+            };
+
+            // Get term
+            let term_bytes = term.to_bytes();
+            let term_ref = match index_reader.store.term_dictionary.get(&term_bytes) {
+                Some(term_ref) => term_ref,
+                None => {
+                    // Term doesn't exist, so will never match
+                    plan.boolean_query.push(BooleanQueryOp::PushEmpty);
+                    plan.score_function.push(ScoreFunctionOp::Literal(0.0f64));
+                    return
+                }
+            };
+
+            plan.boolean_query.push(BooleanQueryOp::PushTermDirectory(field_ref, term_ref));
+            plan.score_function.push(ScoreFunctionOp::TermScorer(field_ref, term_ref, scorer.clone()));
+        }
+        Query::MatchMultiTerm{ref field, ref term_selector, ref scorer} => {
+            // Get field
+            let field_ref = match index_reader.schema().get_field_by_name(field) {
+                Some(field_ref) => field_ref,
+                None => {
+                    // Field doesn't exist, so will never match
+                    plan.boolean_query.push(BooleanQueryOp::PushEmpty);
+                    plan.score_function.push(ScoreFunctionOp::Literal(0.0f64));
+                    return
+                }
+            };
+
+            // Get terms
+            plan.boolean_query.push(BooleanQueryOp::PushEmpty);
+            let mut total_terms = 0;
+            for term_ref in index_reader.store.term_dictionary.select(term_selector) {
+                plan.boolean_query.push(BooleanQueryOp::PushTermDirectory(field_ref, term_ref));
+                plan.boolean_query.push(BooleanQueryOp::Or);
+                plan.score_function.push(ScoreFunctionOp::TermScorer(field_ref, term_ref, scorer.clone()));
+                total_terms += 1;
+            }
+
+            // This query must push only one score value onto the stack.
+            // If we haven't pushed any score operations, Push a literal 0.0
+            // If we have pushed more than one score operations, which will lead to more
+            // than one score value being pushed to the stack, combine the score values
+            // with a combinator operation.
+            match total_terms {
+                0 => plan.score_function.push(ScoreFunctionOp::Literal(0.0f64)),
+                1 => {},
+                _ => plan.score_function.push(ScoreFunctionOp::CombinatorScorer(total_terms, CombinatorScorer::Avg)),
+            }
+        }
+        Query::Conjunction{ref queries} => {
+            plan_query_combinator(index_reader, &mut plan, queries, BooleanQueryOp::And, score, CombinatorScorer::Avg);
+        }
+        Query::Disjunction{ref queries} => {
+            plan_query_combinator(index_reader, &mut plan, queries, BooleanQueryOp::Or, score, CombinatorScorer::Avg);
+        }
+        Query::DisjunctionMax{ref queries} => {
+            plan_query_combinator(index_reader, &mut plan, queries, BooleanQueryOp::Or, score, CombinatorScorer::Max);
+        }
+        Query::Filter{ref query, ref filter} => {
+            plan_query(index_reader, &mut plan, query, score);
+            plan_query(index_reader, &mut plan, filter, false);
+            plan.boolean_query.push(BooleanQueryOp::And);
+        }
+        Query::Exclude{ref query, ref exclude} => {
+            plan_query(index_reader, &mut plan, query, score);
+            plan_query(index_reader, &mut plan, exclude, false);
+            plan.boolean_query.push(BooleanQueryOp::AndNot);
         }
     }
 }
