1f3a5f7d700d8dfa0087f939fc530838ee4379d2
Karl Hobley <karlhobley10@gmail.com>
2016-10-21 11:24:27 +0100

    Split plan_query into two separate functions

--- kite_rocksdb/src/search/planner/mod.rs
+++ kite_rocksdb/src/search/planner/mod.rs
@@ -26,37 +26,32 @@ impl SearchPlan {
 }
 
 
-fn plan_query_combinator(index_reader: &RocksDBIndexReader, mut plan: &mut SearchPlan, queries: &Vec<Query>, join_op: BooleanQueryOp, score: bool, scorer: CombinatorScorer) {
+fn plan_boolean_query_combinator(index_reader: &RocksDBIndexReader, mut plan: &mut SearchPlan, queries: &Vec<Query>, join_op: BooleanQueryOp) {
     match queries.len() {
         0 => {
             plan.boolean_query.push(BooleanQueryOp::PushEmpty);
-            plan.score_function.push(ScoreFunctionOp::Literal(0.0f64));
         }
-        1 =>  plan_query(index_reader, &mut plan, &queries[0], score),
+        1 =>  plan_boolean_query(index_reader, &mut plan, &queries[0]),
         _ => {
             let mut query_iter = queries.iter();
-            plan_query(index_reader, &mut plan, query_iter.next().unwrap(), score);
+            plan_boolean_query(index_reader, &mut plan, query_iter.next().unwrap());
 
             for query in query_iter {
-                plan_query(index_reader, &mut plan, query, score);
+                plan_boolean_query(index_reader, &mut plan, query);
                 plan.boolean_query.push(join_op.clone());
             }
         }
     }
-
-    plan.score_function.push(ScoreFunctionOp::CombinatorScorer(queries.len() as u32, scorer));
 }
 
 
-pub fn plan_query(index_reader: &RocksDBIndexReader,mut plan: &mut SearchPlan, query: &Query, score: bool) {
+fn plan_boolean_query(index_reader: &RocksDBIndexReader, mut plan: &mut SearchPlan, query: &Query) {
     match *query {
         Query::MatchAll{ref score} => {
             plan.boolean_query.push(BooleanQueryOp::PushFull);
-            plan.score_function.push(ScoreFunctionOp::Literal(*score));
         }
         Query::MatchNone => {
             plan.boolean_query.push(BooleanQueryOp::PushEmpty);
-            plan.score_function.push(ScoreFunctionOp::Literal(0.0f64));
         }
         Query::MatchTerm{ref field, ref term, ref scorer} => {
             // Get field
@@ -65,7 +60,6 @@ pub fn plan_query(index_reader: &RocksDBIndexReader,mut plan: &mut SearchPlan, q
                 None => {
                     // Field doesn't exist, so will never match
                     plan.boolean_query.push(BooleanQueryOp::PushEmpty);
-                    plan.score_function.push(ScoreFunctionOp::Literal(0.0f64));
                     return
                 }
             };
@@ -77,13 +71,11 @@ pub fn plan_query(index_reader: &RocksDBIndexReader,mut plan: &mut SearchPlan, q
                 None => {
                     // Term doesn't exist, so will never match
                     plan.boolean_query.push(BooleanQueryOp::PushEmpty);
-                    plan.score_function.push(ScoreFunctionOp::Literal(0.0f64));
                     return
                 }
             };
 
             plan.boolean_query.push(BooleanQueryOp::PushTermDirectory(field_ref, term_ref));
-            plan.score_function.push(ScoreFunctionOp::TermScorer(field_ref, term_ref, scorer.clone()));
         }
         Query::MatchMultiTerm{ref field, ref term_selector, ref scorer} => {
             // Get field
@@ -92,17 +84,106 @@ pub fn plan_query(index_reader: &RocksDBIndexReader,mut plan: &mut SearchPlan, q
                 None => {
                     // Field doesn't exist, so will never match
                     plan.boolean_query.push(BooleanQueryOp::PushEmpty);
-                    plan.score_function.push(ScoreFunctionOp::Literal(0.0f64));
                     return
                 }
             };
 
             // Get terms
             plan.boolean_query.push(BooleanQueryOp::PushEmpty);
-            let mut total_terms = 0;
             for term_ref in index_reader.store.term_dictionary.select(term_selector) {
                 plan.boolean_query.push(BooleanQueryOp::PushTermDirectory(field_ref, term_ref));
                 plan.boolean_query.push(BooleanQueryOp::Or);
+            }
+        }
+        Query::Conjunction{ref queries} => {
+            plan_boolean_query_combinator(index_reader, &mut plan, queries, BooleanQueryOp::And);
+        }
+        Query::Disjunction{ref queries} => {
+            plan_boolean_query_combinator(index_reader, &mut plan, queries, BooleanQueryOp::Or);
+        }
+        Query::DisjunctionMax{ref queries} => {
+            plan_boolean_query_combinator(index_reader, &mut plan, queries, BooleanQueryOp::Or);
+        }
+        Query::Filter{ref query, ref filter} => {
+            plan_boolean_query(index_reader, &mut plan, query);
+            plan_boolean_query(index_reader, &mut plan, filter);
+            plan.boolean_query.push(BooleanQueryOp::And);
+        }
+        Query::Exclude{ref query, ref exclude} => {
+            plan_boolean_query(index_reader, &mut plan, query);
+            plan_boolean_query(index_reader, &mut plan, exclude);
+            plan.boolean_query.push(BooleanQueryOp::AndNot);
+        }
+    }
+}
+
+
+fn plan_score_function_combinator(index_reader: &RocksDBIndexReader, mut plan: &mut SearchPlan, queries: &Vec<Query>, scorer: CombinatorScorer) {
+    match queries.len() {
+        0 => {
+            plan.score_function.push(ScoreFunctionOp::Literal(0.0f64));
+        }
+        1 =>  plan_score_function(index_reader, &mut plan, &queries[0]),
+        _ => {
+            let mut query_iter = queries.iter();
+            plan_score_function(index_reader, &mut plan, query_iter.next().unwrap());
+
+            for query in query_iter {
+                plan_score_function(index_reader, &mut plan, query);
+            }
+        }
+    }
+
+    plan.score_function.push(ScoreFunctionOp::CombinatorScorer(queries.len() as u32, scorer));
+}
+
+
+fn plan_score_function(index_reader: &RocksDBIndexReader, mut plan: &mut SearchPlan, query: &Query) {
+    match *query {
+        Query::MatchAll{ref score} => {
+            plan.score_function.push(ScoreFunctionOp::Literal(*score));
+        }
+        Query::MatchNone => {
+            plan.score_function.push(ScoreFunctionOp::Literal(0.0f64));
+        }
+        Query::MatchTerm{ref field, ref term, ref scorer} => {
+            // Get field
+            let field_ref = match index_reader.schema().get_field_by_name(field) {
+                Some(field_ref) => field_ref,
+                None => {
+                    // Field doesn't exist, so will never match
+                    plan.score_function.push(ScoreFunctionOp::Literal(0.0f64));
+                    return
+                }
+            };
+
+            // Get term
+            let term_bytes = term.to_bytes();
+            let term_ref = match index_reader.store.term_dictionary.get(&term_bytes) {
+                Some(term_ref) => term_ref,
+                None => {
+                    // Term doesn't exist, so will never match
+                    plan.score_function.push(ScoreFunctionOp::Literal(0.0f64));
+                    return
+                }
+            };
+
+            plan.score_function.push(ScoreFunctionOp::TermScorer(field_ref, term_ref, scorer.clone()));
+        }
+        Query::MatchMultiTerm{ref field, ref term_selector, ref scorer} => {
+            // Get field
+            let field_ref = match index_reader.schema().get_field_by_name(field) {
+                Some(field_ref) => field_ref,
+                None => {
+                    // Field doesn't exist, so will never match
+                    plan.score_function.push(ScoreFunctionOp::Literal(0.0f64));
+                    return
+                }
+            };
+
+            // Get terms
+            let mut total_terms = 0;
+            for term_ref in index_reader.store.term_dictionary.select(term_selector) {
                 plan.score_function.push(ScoreFunctionOp::TermScorer(field_ref, term_ref, scorer.clone()));
                 total_terms += 1;
             }
@@ -119,23 +200,30 @@ pub fn plan_query(index_reader: &RocksDBIndexReader,mut plan: &mut SearchPlan, q
             }
         }
         Query::Conjunction{ref queries} => {
-            plan_query_combinator(index_reader, &mut plan, queries, BooleanQueryOp::And, score, CombinatorScorer::Avg);
+            plan_score_function_combinator(index_reader, &mut plan, queries, CombinatorScorer::Avg);
         }
         Query::Disjunction{ref queries} => {
-            plan_query_combinator(index_reader, &mut plan, queries, BooleanQueryOp::Or, score, CombinatorScorer::Avg);
+            plan_score_function_combinator(index_reader, &mut plan, queries, CombinatorScorer::Avg);
         }
         Query::DisjunctionMax{ref queries} => {
-            plan_query_combinator(index_reader, &mut plan, queries, BooleanQueryOp::Or, score, CombinatorScorer::Max);
+            plan_score_function_combinator(index_reader, &mut plan, queries, CombinatorScorer::Max);
         }
         Query::Filter{ref query, ref filter} => {
-            plan_query(index_reader, &mut plan, query, score);
-            plan_query(index_reader, &mut plan, filter, false);
-            plan.boolean_query.push(BooleanQueryOp::And);
+            plan_score_function(index_reader, &mut plan, query);
         }
         Query::Exclude{ref query, ref exclude} => {
-            plan_query(index_reader, &mut plan, query, score);
-            plan_query(index_reader, &mut plan, exclude, false);
-            plan.boolean_query.push(BooleanQueryOp::AndNot);
+            plan_score_function(index_reader, &mut plan, query);
         }
+    }
+}
+
+
+pub fn plan_query(index_reader: &RocksDBIndexReader, mut plan: &mut SearchPlan, query: &Query, score: bool) {
+    plan_boolean_query(index_reader, plan, query);
+
+    if score {
+        plan_score_function(index_reader, plan, query);
+    } else {
+        plan.score_function.push(ScoreFunctionOp::Literal(0.0f64));
     }
 }
