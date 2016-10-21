cc014802e5c503279ef749e9f568fe34666921ba
Karl Hobley <karlhobley10@gmail.com>
2016-10-21 14:47:46 +0100

    Handle rocksdb write errors in chunk merger

--- kite_rocksdb/src/errors.rs
+++ kite_rocksdb/src/errors.rs
@@ -13,3 +13,46 @@ impl RocksDBReadError {
         }
     }
 }
+
+
+#[derive(Debug)]
+enum RocksDBWriteOperation {
+    Put,
+    Merge,
+    Delete,
+}
+
+
+#[derive(Debug)]
+pub struct RocksDBWriteError {
+    key: Vec<u8>,
+    operation: RocksDBWriteOperation,
+    message: String,
+}
+
+
+impl RocksDBWriteError {
+    pub fn new_put(key: Vec<u8>, message: String) -> RocksDBWriteError {
+        RocksDBWriteError {
+            key: key,
+            operation: RocksDBWriteOperation::Put,
+            message: message,
+        }
+    }
+
+    pub fn new_merge(key: Vec<u8>, message: String) -> RocksDBWriteError {
+        RocksDBWriteError {
+            key: key,
+            operation: RocksDBWriteOperation::Merge,
+            message: message,
+        }
+    }
+
+    pub fn new_delete(key: Vec<u8>, message: String) -> RocksDBWriteError {
+        RocksDBWriteError {
+            key: key,
+            operation: RocksDBWriteOperation::Delete,
+            message: message,
+        }
+    }
+}
