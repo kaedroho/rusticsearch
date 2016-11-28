a208ebe7fb7156b2d6d0ba15bf5b4c7d35790c18
Karl Hobley <karlhobley10@gmail.com>
2016-11-28 19:40:45 +0000

    Implement FieldValue.as_json()

--- kite/src/document.rs
+++ kite/src/document.rs
@@ -1,5 +1,6 @@
 use std::collections::HashMap;
 
+use rustc_serialize::json::Json;
 use chrono::{DateTime, UTC, Timelike};
 use byteorder::{WriteBytesExt, BigEndian};
 
@@ -47,6 +48,15 @@ impl FieldValue {
                 bytes.write_i64::<BigEndian>(timestamp_with_micros).unwrap();
                 bytes
             }
+        }
+    }
+
+    pub fn as_json(&self) -> Json {
+        match *self {
+            FieldValue::String(ref string) => Json::String(string.clone()),
+            FieldValue::Boolean(value) => Json::Boolean(value),
+            FieldValue::Integer(value) => Json::I64(value),
+            FieldValue::DateTime(value) => Json::String(value.to_rfc3339()),
         }
     }
 }
